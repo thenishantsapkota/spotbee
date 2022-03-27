216aff5264d688ae3bb5aed95ce28055590f95e3
Nishant Sapkota <snishant306@gmail.com>
2022-03-27 20:16:06 +0545

    feat: add a way to get raw track titles without converting them to youtube urls

---
+++ examples/get_track_titles.py
@@ -0,0 +1,14 @@
+import asyncio
+
+import spotbee
+
+instance = spotbee.SpotBee("spotify_client_id", "spotify_client_secret")
+
+
+async def track_titles():
+    tracks = await instance.get_track_titles("spotify_url")
+    # Get Spotify Client ID, Spotify Client Secret from https://developer.spotify.com
+    print(tracks)
+
+
+asyncio.run(track_titles())
